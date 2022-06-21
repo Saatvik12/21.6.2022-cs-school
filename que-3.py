8b2f38137414c06b08f4efe11b58b2039b41a961
Saatvik12 <90774068+Saatvik12@users.noreply.github.com>
2022-06-21 23:53:46 +0530

    Create que-3.py

---
+++ que-3.py
@@ -0,0 +1,10 @@
+#3rd code
+num = int(input('enter a number'))
+if num%3 == 0:
+  print('number is divisible by 3')
+else:
+  print('number is not divisible by 3')
+if num%5 ==0:
+  print('number is divisible by 5')
+else:
+  print(' number is not divisible by 5')
